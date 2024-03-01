15987c4ab66c5327d1211b891f93f41e770dfdb5
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-03-01 15:41:33 +0800

    [5] Create model and database
    Creating model name and show in database. db.sqlite3 as database

--- todo_api/api/models.py
+++ todo_api/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.body[0:50]
+    
+    class Meta: 
+        ordering = ['-updated']
