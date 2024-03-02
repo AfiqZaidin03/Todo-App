89e24df0478074c2cfbaa7de6ad8fe511ba47c32
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-03-02 10:31:40 +0800

    Add create and update api
    Adding create and update api for task and the route

--- todo_api/api/urls.py
+++ todo_api/api/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes),
     path('notes/', views.getTasks),
+    path('notes/create/', views.createTask),
+    path('notes/<str:pk>/update/', views.updateTask),
     path('notes/<str:pk>/', views.getTask)
 ]
